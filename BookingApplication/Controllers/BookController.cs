30c3606a52094cfcc2795d83ff2e91c25a46c665
adam.bubula <adam.bubula@flotman.pl>
2021-10-13 13:48:02 +0200

    search view

---
+++ BookingApplication/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using BookingApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View(new SearchViewModel() { StartDate = DateTime.Today, EndDate = DateTime.Today.AddDays(1) });
+        }
+    }
+}
+
---
+++ BookingApplication/Models/SearchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApplication.Models
+{
+    public class SearchViewModel
+    {
+        //main
+        public string Name { get; set; }       
+        public int People { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        //additional filters
+        public int Beds { get; set; }
+        public double MinRate { get; set; }
+        public int MinStars { get; set; }        
+        public bool Balcony { get; set; }
+        public bool PrivateBathroom { get; set; }
+        public bool Kitchen { get; set; }
+        public bool Tv { get; set; }
+        public decimal MaxNightPrice { get; set; }
+    }
+}
